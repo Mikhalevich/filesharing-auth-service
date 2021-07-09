c41ca1e2bb56c81bb58eacad4ad610c89c7a4169
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2021-07-09 19:04:33 +0300

    add sleep 2 sec for waiting db connection

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+sleep 2
+
 goose -dir=db/migrations postgres "user=postgres password=123456 dbname=filesharing host=dbpg port=5432 sslmode=disable" up
 
 ./filesharing-auth-service
